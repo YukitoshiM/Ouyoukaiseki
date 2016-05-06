ff78140a762f3d2ebb6dcd74b1f673a2258e4574
YukitoshiM <amasihsotikuy@hotmail.co.jp>
2016-05-06 12:24:41 +0900

    aaaa

--- homewrok1.py
+++ homewrok1.py
@@ -11,77 +11,62 @@ import numpy as np
 
 #changed the input
 n = 10.0 # input for num of x
-m = 50.0 # input for num of time 
-x_minus = 0.0 # input of starting point 
+m = 20.0 # input for num of time 
+x_minus = -10.0 # input of starting point 
 x_plus = 10.0 #input of ending point 
-r = 5.0 # any given for absolute value of x
+r =100 # any given for absolute value of x
 c = 5.0 # any constant value 
-time = 1.0 #in sec
+time = 4 #in sec
 tau = time/m # the variable "time" divides into num of m
 h = (x_plus-x_minus)/n # the  variable "x1 to x2
 alpha = (c*tau)/(h*h) # 差分方程式のalpha
 
 y = np.arange(0, time, tau)
-x = np.arange(0, x_plus, h)
+x = np.arange(x_minus, x_plus+h, h)
 X, Y = np.meshgrid(x, y)
 fig = plt.figure()
 ax = Axes3D(fig)
 
-temp_U_n_plus_1 = 0
-
 k = 0
-j = x_minus + h
+j = x_minus 
 
 arr_2d = []
 
-#Adding num. of k arrays in the arr2_d to prepared for the 2d array data structure
-while k < time:
-    arr_2d.append([])
-    k = k + tau
+#arr_2d[0].append((-1)*2*j*(1-j)) #U0 = -U1
 
-arr_2d[0].append((-1)*2*j*(1-j)) #U0 = -U1
-
-while j < x_plus:
-    if j < r:
-        arr_2d[0].append(2*j*(1-j)) #if Xj < R then u(Xj,0)=f(x) = 2*xj(1-xj)
-    else:
-        arr_2d[0].append(0) # if Xj >= Rtheb u(Xj,0) = 0
-    j = j + h
-
-j = j - h
-
-#setting U_N+1
-#if j < r:
-#    arr_2d[0].append((-1)*2*(j-h)*(1-(j-h))) #if Xj < R then u(Xj,0)=f(x) = 2*xj(1-xj)
-#else:
-#    arr_2d[0].append(0) # if Xj >= Rtheb u(Xj,0) = 0
-    
-
-k = tau
-j = 0
-k_counter = 1
-j_counter = 0
-u_new = 0
-temp = 0.
-
-#fixed by multiplying alpha for the third term
-while k < time:
-    while j < x_plus:
+def f_of_x(j, r, h, length):
+    if abs(j) < r:
         if j == 0:
-            arr_2d[k_counter].append((((-1)*(alpha*arr_2d[(k_counter-1)][(j_counter)]+(1-2*alpha)*arr_2d[(k_counter-1)][(j_counter + 1)]+alpha*arr_2d[(k_counter-1)][j_counter+2]))))
-        try:
-            arr_2d[k_counter].append(alpha*arr_2d[(k_counter-1)][(j_counter-1)]+(1-2*alpha)*arr_2d[(k_counter-1)][(j_counter)]+alpha*arr_2d[(k_counter-1)][j_counter+1])
-        except IndexError:
-            break
-            #arr_2d[k_counter].append(alpha*arr_2d[(k_counter-1)][(j_counter-2)]+(1-2*alpha)*arr_2d[(k_counter-1)][(j_counter-1)]+alpha*arr_2d[(k_counter-1)][j_counter])
-        j = j + h        
-        j_counter = j_counter + 1
-    j = 0
-    j_counter = 0
-    k = k + tau
-    k_counter = k_counter + 1
+            j = j + h
+        elif j == length:
+            j = j - h
+        return 2*j*(1-j)
+    return 0
     
+def calc(u,j,r,alpha):
+    if abs(j) < r:
+        if j == 0:
+            return (-1)*(alpha*u[j]+(1-2*alpha)*u[(j+1)]+alpha*u[j+2])
+        elif j == (len(u) - 1):
+            return (-1)*(alpha*u[j-2]+(1-2*alpha)*u[(j-1)]+alpha*u[j])
+        return alpha*u[j-1]+(1-2*alpha)*u[j]+alpha*u[j+1]
+    return 0
+
+while k < time:
+    arr_2d.append(x)
+    k = k + tau
+
 Z = np.array(arr_2d)
-ax.plot_wireframe(X,Y,Z)
 print(Z)
+for i, s in enumerate(Z[0]):
+    Z[0][i] = f_of_x(s,r,h,len(Z[0]))
+
+for i, s in enumerate(Z):
+    if i != 0:
+        for l, k in enumerate(s):
+            Z[i][l] = calc(Z[i-1],l,r,alpha)
+
+print(Z)
+
+ax.plot_wireframe(X,Y,Z)
 plt.show()
